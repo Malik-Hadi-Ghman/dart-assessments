68731ef5cb2d942470c35f0e7986b4c0265b76ec
Malik-Hadi-Ghman <stumhg7755@gmail.com>
2026-04-23 14:50:39 +0300

    Create assignment12.dart

---
+++ assignment12.dart
@@ -0,0 +1,24 @@
+class Employee {
+  void work() {
+    print("Employee working");
+  }
+}
+
+class Manager extends Employee {
+  @override
+  void work() {
+    print("Manager managing");
+  }
+}
+
+class SeniorManager extends Manager {
+  @override
+  void work() {
+    print("Senior Manager leading");
+  }
+}
+
+void main() {
+  Employee e = SeniorManager();
+  e.work();
+}
