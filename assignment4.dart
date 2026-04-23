6dd7b0f7a02d2df5671f5964de68c00b3889cffe
Malik-Hadi-Ghman <stumhg7755@gmail.com>
2026-04-23 14:44:48 +0300

    Update assignment4.dart

--- assignment4.dart
+++ assignment4.dart
@@ -1,17 +1,12 @@
 void main() {
-  // Temperature with decimal precision
   double temperature = 36.5;
 
-  // Number of bus passengers
   int passengers = 25;
 
-  // Residential address
   String address = "Sana'a, Yemen";
 
-  // Dark mode state
   bool isDarkMode = true;
 
-  // Print values
   print("Temperature: $temperature");
   print("Passengers: $passengers");
   print("Address: $address");
